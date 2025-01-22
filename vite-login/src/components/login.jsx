f884703e64da9d20e7c45071a58d5ea0b38293f2
mr-dharaviya-07 <ravidharaviya2624@gmail.com>
2025-01-22 09:52:15 +0530

    some Changes

--- vite-login/src/components/login.jsx
+++ vite-login/src/components/login.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Validation } from './validation';
--- vite-login/src/components/profile.jsx
+++ vite-login/src/components/profile.jsx
@@ -1,13 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
 
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket, faUserAlt } from "@fortawesome/free-solid-svg-icons";
-
-// import { Response } from "./response";
 
 const Profile = () => {
 
@@ -33,29 +28,29 @@ const Profile = () => {
 
 
     }, [userId]);
-    
-    
+
+
 
     const fetchData = async (userId) => {
-        
+
         try {
             const res = await fetch(`http://localhost:4000/get-profile/${userId}`);
 
             const userData = await res.json();
-    
+
             // console.log(userData)
             setName(userData.name);
             setprofilePicture(`http://localhost:4000/uploads/${userData.profile_picture}`);
-            
+
         }
         catch (error) {
             console.log(error);
             // setResponseError(error);
         }
-        
+
     }
 
-    
+
     const showProfile = () => {
         navigate("/showProfile")
     }
@@ -67,18 +62,13 @@ const Profile = () => {
             <nav className="absolute bg-gray-800 w-full h-14 shadow-lg text-white flex justify-center items-center">
                 <button onClick={showProfile} title="Profile" className="flex justify-center items-center w-12 h-12  bg-green-500 p-2 rounded-full text-lg text-white absolute right-32">
                     {/* { <img src={`${profilePicture}`} alt="" /> && <FontAwesomeIcon icon={faUserAlt} style={{ color: 'rgb(0, 8, 8)', width: "22px", height: "22px" }} /> } */}
-                    <img src={profilePicture} alt="Profile" className=" w-52 scale-150 aspect-square object-cover object-top rounded-full "  />
+                    <img src={profilePicture} alt="Profile" className=" w-52 scale-150 aspect-square object-cover object-top rounded-full " />
                 </button>
                 <button onClick={logout} title="Logout" className="flex justify-center items-center w-24 h-12 bg-blue-500 p-2 rounded-xl text-lg text-white absolute right-5">
                     {/* <FontAwesomeIcon icon={faArrowRightFromBracket} style={{ color: 'rgb(0, 8, 8)', width: "25px", height: "25px" }} /> */} Logout
                 </button>
             </nav>
             <div className="bg-white h-screen flex flex-col justify-center items-center">
-
-                {/* <div className={`m-10 w-56 bg-green-500 text-black text-center py-2 rounded-lg shadow-lg transition ease-in ${showAlert ? "opacity-100 " : "opacity-0"}`}>
-                <strong>Success !</strong> Login successful!
-                </div> */}
-
 
 
                 <div className=" h-72 w-96 flex flex-col justify-around items-center rounded">
